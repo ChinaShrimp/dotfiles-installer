9de60efdb11193eb7b96280b5c461d1103da3b8a
Lyon Liang <lyon.liang12@gmail.com>
2018-01-29 15:51:17 +0000

    add rcup fix

--- install.sh
+++ install.sh
@@ -65,4 +65,4 @@ cp -r zsh ~/dotfiles-local/
 export ZSH=$HOME/dotfiles-local/zsh/oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-env RCRC=$HOME/dotfiles/rcrc rcup
+env RCRC=$HOME/dotfiles/rcrc sudo rcup -f
